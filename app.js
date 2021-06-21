48571ffb5bbba2998ef8052cafadc88247820b43
Sushant Bahirat <sushantbahirat40@gmail.com>
2021-06-21 16:22:44 +0530

    Add secret file and connect with mongoDB atlas server

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mongooose = require("mongoose"); 
 const ejs = require("ejs");
 const _ = require("lodash");
+const {USERNAME, PASSWORD} = require("./secret.js");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -17,7 +18,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 //connecting to the URL
-const URL = "mongodb://localhost:27017";
+const URL = `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.hxqnm.mongodb.net`;
 
 mongooose.connect(URL + "/blogDB", {
     useNewUrlParser: true,
